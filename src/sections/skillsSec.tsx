82ccb3b13d431c6b9c60e4c3f1d17aa8f85d5868
JosBena <jbenavides.business@gmail.com>
2024-03-29 22:44:01 -0400

    customizing The skills and Colors

--- src/sections/skillsSec.tsx
+++ src/sections/skillsSec.tsx
@@ -1,48 +1,87 @@
-import { Container } from "react-bootstrap";
+import { Col, Container } from "react-bootstrap";
 import { HoverList } from "../components/hoverList";
+import { Bootstrap, Github } from "react-bootstrap-icons";
+//react icons
 import { IoLogoJavascript } from "react-icons/io5";
-import { Bootstrap } from "react-bootstrap-icons";
+import {
+  SiTypescript,
+  SiUnrealengine,
+  SiGodotengine,
+  SiFirebase,
+  SiTailwindcss,
+  SiOculus,
+} from "react-icons/si";
+import { TbBrandCSharp } from "react-icons/tb";
+import { BiLogoCPlusPlus } from "react-icons/bi";
+import { FaJava, FaPhp, FaUnity, FaReact } from "react-icons/fa";
+import { FaDocker } from "react-icons/fa6";
+import { GrMysql } from "react-icons/gr";
 
+const cssStyle =
+  "h5 g-col-1 text-center mb-3 fw-bold fs-4 d-flex align-items-center ";
+const SkillList = ({ list, title }: any) => {
+  return (
+    <div>
+      <div className="d-flex bg-altlight pt-3 ps-5 align-items-center gap-3 mt-3">
+        <Col sm="4">
+          <p className={cssStyle}> {title} </p>
+        </Col>
+        <Col>
+          <HoverList itemList={list} isActive={true}></HoverList>
+        </Col>
+      </div>
+    </div>
+  );
+};
 export const Skills = () => {
   const languageList = [
+    { icon: <TbBrandCSharp />, title: "C#" },
+    { icon: <BiLogoCPlusPlus />, title: "C++" },
     { icon: <IoLogoJavascript />, title: "javascript" },
-    { icon: <Bootstrap />, title: "bootstrap" },
-    { icon: <Bootstrap />, title: "bootstrap" },
+    { icon: <SiTypescript />, title: "TypeScript" },
+    { icon: <FaJava />, title: "Java" },
+    { icon: <FaPhp />, title: "PHP" },
   ];
   const gameEngineList = [
-    { icon: <IoLogoJavascript />, title: "javascript" },
-    { icon: <IoLogoJavascript />, title: "javascript" },
+    { icon: <FaUnity />, title: "Unity" },
+    { icon: <SiUnrealengine />, title: "Unreal Engine" },
+    { icon: <SiGodotengine />, title: "Godot" },
+  ];
+  const libraries = [
+    { icon: <FaReact />, title: "React" },
+    { icon: <SiFirebase />, title: "Firebase" },
   ];
   const databaseList = [
-    { icon: <IoLogoJavascript />, title: "javascript" },
-    { icon: <IoLogoJavascript />, title: "javascript" },
+    { icon: <SiFirebase />, title: "Firestore" },
+    { icon: <GrMysql />, title: "MySql" },
   ];
   const toolsList = [
-    { icon: <IoLogoJavascript />, title: "javascript" },
-    { icon: <IoLogoJavascript />, title: "javascript" },
+    { icon: <Github />, title: "Github" },
+    { icon: <FaDocker />, title: "Docker" },
+    { icon: <FaUnity />, title: "D.O.T.S" },
   ];
   const frameworksList = [
-    { icon: <IoLogoJavascript />, title: "javascript" },
-    { icon: <IoLogoJavascript />, title: "javascript" },
+    { icon: <Bootstrap />, title: "Bootstrap" },
+    { icon: <SiTailwindcss />, title: "Tailwind" },
+  ];
+  const gameTypes = [
+    { icon: <SiOculus />, title: "VR/XR" },
+    { icon: <FaUnity />, title: "3d/2d" },
   ];
 
-  const cssStyle = "h5 g-col-1 text-center mb-3 fw-semibold";
   return (
-    <section id="skills" className="pt-0 px-0">
-      <Container fluid>
-        <div className="h1 text-center py-3 pt-4 fw-bold text-golden">
+    <section id="skills" className="p-0 bg-dark pb-3">
+      <Container fluid className="">
+        <div className="h1 text-center py-3 pt-4 fw-bold text-white ">
           Skills
         </div>
-        <p className={cssStyle}> Languages </p>
-        <HoverList itemList={languageList} isActive={true}></HoverList>
-        <p className={cssStyle}> GameEngines </p>
-        <HoverList itemList={gameEngineList} isActive={true}></HoverList>
-        <p className={cssStyle}> Databases </p>
-        <HoverList itemList={databaseList} isActive={true}></HoverList>
-        <p className={cssStyle}> Tools </p>
-        <HoverList itemList={toolsList} isActive={true}></HoverList>
-        <p className={cssStyle}> FrameWorks </p>
-        <HoverList itemList={frameworksList} isActive={true}></HoverList>
+        <SkillList list={languageList} title="Languages" />
+        <SkillList list={gameEngineList} title="GameEngines" />
+        <SkillList list={frameworksList} title="FrameWorks" />
+        <SkillList list={libraries} title="Libraries (Frontend/Backend)" />
+        <SkillList list={databaseList} title="Databases" />
+        <SkillList list={toolsList} title="Tools" />
+        <SkillList list={gameTypes} title="Game Types " />
       </Container>
     </section>
   );
