7898a99651776e48311d29a755bc3e525f97f823
JosBena <jbenavides.business@gmail.com>
2024-04-03 10:22:54 -0400

    Updated About Me

--- src/sections/aboutMeSec.tsx
+++ src/sections/aboutMeSec.tsx
@@ -9,28 +9,48 @@ const AboutMeMessage = () => {
         game development, illustration, and web design/development.
       </p>
       <p>
-        Most of my skills relating to IT come from projects I've created and
-        led, which has improved my leadership, and organizing skills. Some of
-        these projects include creating some high-definition models in Houdini,
-        others include modding Games and creating games for Unity. I also have
-        experience with creating game models in Blender and illustrations in
-        Clipstudio Paint. In regards to my skills Related to web design come
-        similarly from projects I've created and led. In these projects I've
-        developed both the front-end and back-end of the websites using PHP,
-        JavaScript, MySQL, HTML, and CSS frameworks like bootstrap.
+        Most of my skills relating to IT come from
+        <a
+          href="#projects"
+          className="text-white fw-bold"
+          style={{ textDecoration: "none" }}
+        >
+          {" "}
+          Projects{" "}
+        </a>
+        I've created and led during my time in University, which has improved my
+        leadership, and organizing skills. Some of these projects include
+        creating some high-definition models in Houdini, others include modding
+        Games and creating games for Unity. I also have experience with creating
+        game models in Blender and illustrations in Clipstudio Paint. In regards
+        to my skills Related to web design, they come from Projects in
+        University and from practice for creating the website for my Porfolio.
+        In these projects I've developed both the front-end and back-end of the
+        websites using PHP, JavaScript, MySQL, HTML, and CSS frameworks like
+        bootstrap. Recently, I've also learned React and how to use it with
+        booth Bootstrap and Tailwind.
       </p>
       <p>
-        My short term goals are to obtain more experience with internships &
-        work, while also improving my skills in the field of IT, game
-        development, and Illustration. My mid-term goals are to work in a game
-        development company in the future as the industry is currently booming,
-        in which I could not only create the games, but also illustrate their
-        art and make 3D/2D models for them. Moreover, as I understand How to
-        create VR games I would also like to work in the VR industry as Its a
-        booming industry at the moment. Furthermore, as I'm able to create
-        websites I would also be able to both maintain them and improve them in
-        both ends. Lastly, My Long-term goal is to create my own studio for
-        videogames and create the games that I desire.
+        <b>Goals:</b>
+      </p>
+      <hr></hr>
+      <p>
+        <b>Short-Term:</b> Obtain more experience with work, while also
+        improving my skills in the field of IT, game development, web design,
+        and Illustration.
+      </p>
+      <p>
+        <b>Mid-Term:</b> Work in a game development company in the future as the
+        industry is currently booming, in which I could not only create the
+        games, but also illustrate their art and make 3D/2D models for them.
+        Moreover, as I understand How to create VR games I would also like to
+        work in the VR industry as Its a booming industry at the moment.
+        Furthermore, as I'm able to create websites I would also be able to both
+        maintain them and improve them in both ends.
+      </p>
+      <p>
+        <b>Long-term:</b> Create my own studio for videogames and create the
+        games that I desire.
       </p>
     </>
   );
